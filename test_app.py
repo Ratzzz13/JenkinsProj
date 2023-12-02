5ed8159c87978463f7d34cd0052a5194af76a48d
rathiesh <rathieshsekaran@gmail.com>
2023-12-02 19:34:08 +0530

    deployment

--- test_app.py
+++ test_app.py
@@ -13,5 +13,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = quote('Hello, World! This is a simple Flask app newone.')
+    expected_text = quote('newchangesdeployed')
     assert expected_text.encode() in response.data
